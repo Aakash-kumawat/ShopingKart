c8c0b2db65be1b755fd985e1da6c7dbf0216a383
AAKASH KUMAWAT <43673230+Aakash-kumawat@users.noreply.github.com>
2022-07-10 17:48:59 +0530

    Main

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
-import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ModuleWithProviders } from '@angular/core';
+import { LoginComponent } from './login/login.component';
+import { ViewProductsComponent } from './view-products/view-products.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LoginComponent },
+    {path:'/view-products',component : ViewProductsComponent},
+    {path:'**',component:LoginComponent}
 
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
+];
+export const routing: ModuleWithProviders<any> = RouterModule.forRoot(routes);
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { ViewProductsComponent } from './view-products/view-products.component';
-
+import{HttpClientModule} from '@angular/common/http';
+import { ViewCartComponent } from './view-cart/view-cart.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    ViewProductsComponent
+    ViewProductsComponent,
+    ViewCartComponent
   
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule,
+    FormsModule,HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
