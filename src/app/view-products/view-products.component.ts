a6c4f1e50fc83cfb3b31777bcc5575eca8e890cd
AAKASH KUMAWAT <43673230+Aakash-kumawat@users.noreply.github.com>
2022-07-10 16:31:22 +0530

    Cart App

---
+++ src/app/view-products/view-products.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-view-products',
+  templateUrl: './view-products.component.html',
+  styleUrls: ['./view-products.component.css']
+})
+export class ViewProductsComponent implements OnInit {
+  products: any[];
+  public showMsgDiv: boolean = false;
+  constructor() {
+    this.products=[];
+   }
+
+  ngOnInit(): void {
+    this.products = [
+      { "productId": "P101", "productName": "Lamborghini Gallardo Spyder", "categoryId": 1, "price": 18000000, "quantityAvailable": 10 },
+     { "productId": "P102", "productName": "Ben Sherman Mens Necktie Silk Tie", "categoryId": 2, "price": 1847, "quantityAvailable": 20 },
+     { "productId": "P103", "productName": "BMW Z4", "categoryId": 1, "price": 6890000, "quantityAvailable": 10 },
+     { "productId": "P104", "productName": "Samsung Galaxy S4", "categoryId": 3, "price": 38800, "quantityAvailable": 100 }
+   ]
+    
+  }
+
+}
